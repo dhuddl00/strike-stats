b8252905aef5510da39cb64e0acc19c8211f5a3f
Dan <dhuddl00@gmail.com>
2015-06-13 12:23:03 -0500

    updated password in script so that it would run successfully

--- utils/create_github_repo.py
+++ utils/create_github_repo.py
@@ -2,6 +2,6 @@ import requests, json
 
 github_url = "https://api.github.com/user/repos"
 data = json.dumps({'name':'strike-stats', 'description':'App to keep track of pitcher performance.'}) 
-r = requests.post(github_url, data, auth=('dhuddl00@gmail.com', '*****'))
+r = requests.post(github_url, data, auth=('dhuddl00@gmail.com', '*******'))
 
-print r.json
+print r.text
